4c83f2355aafdbb99d9037b4e0d2cdd19ed8398c
김호종 <herohjk@naver.com>
2022-01-10 12:50:54 +0900

    초기 설정

---
+++ DoItSomeday/MainListNode.swift
@@ -0,0 +1,20 @@
+//
+//  MainListNode.swift
+//  DoItSomeday
+//
+//  Created by 김호종 on 2022/01/10.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class MainListNode: ASDKViewController<ASDisplayNode> {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+
+}
+
---
+++ DoItSomedayTests/DoItSomedayTests.swift
@@ -0,0 +1,23 @@
+//
+//  DoItSomedayTests.swift
+//  DoItSomedayTests
+//
+//  Created by 김호종 on 2022/01/10.
+//
+
+import XCTest
+@testable import DoItSomeday
+
+class DoItSomedayTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws { }
+
+    func testExample() throws { }
+
+    func testPerformanceExample() throws {
+        self.measure { }
+    }
+
+}
